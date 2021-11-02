c2dcf7e75c4f35da62c49e6030077b7181c4af3d
joandy <joandyalejogarcia@outlook.com>
2021-11-02 19:10:51 -0400

    Improved the unit changing feature by adding a function that converts the units from imperial to metric units, instead of fetching the api

--- src/componets/Search.js
+++ src/componets/Search.js
@@ -6,16 +6,15 @@ import logo from '../assets/icons/svg/logo.svg';
 
 const Search = () => {
 
-    const [unit, setUnit] = useState('metric');
     const [city, setCity] = useState('');
-    const {requestData} = useApiFetch(`https://api.openweathermap.org/data/2.5/weather?${"q=" + city}&appid=8d342f682d66b6e1370bc79bd312bcd2&units=${unit}`);
+    const {requestData} = useApiFetch(`https://api.openweathermap.org/data/2.5/weather?${"q=" + city}&appid=8d342f682d66b6e1370bc79bd312bcd2&units=imperial`);
     const {requestData: onLoadFetch} = useApiFetch();
 
     useEffect(() =>{
         const geoLoactionHandler = async () => {
             if (navigator.geolocation){
                 await navigator.geolocation.getCurrentPosition(position => {
-                    onLoadFetch(`https://api.openweathermap.org/data/2.5/weather?${"&lat=" + position.coords.latitude  + "&lon=" + position.coords.longitude}&appid=8d342f682d66b6e1370bc79bd312bcd2&units=${unit}`)
+                    onLoadFetch(`https://api.openweathermap.org/data/2.5/weather?${"&lat=" + position.coords.latitude  + "&lon=" + position.coords.longitude}&appid=8d342f682d66b6e1370bc79bd312bcd2&units=imperial`)
                 });
             }else{
                 alert('navigator geoloacation not allowed :(')
@@ -33,7 +32,7 @@ const Search = () => {
         if (city === ''){
             return
         }
-       requestData(`https://api.openweathermap.org/data/2.5/weather?${"q=" + city}&appid=8d342f682d66b6e1370bc79bd312bcd2&units=${unit}`)
+       requestData(`https://api.openweathermap.org/data/2.5/weather?${"q=" + city}&appid=8d342f682d66b6e1370bc79bd312bcd2&units=imperial`)
         setCity('')
     }
 
--- src/hooks/useApiFetch.js
+++ src/hooks/useApiFetch.js
@@ -3,28 +3,30 @@ import {apiActions} from "../store/api-slice";
 import {useCallback} from 'react';
 
 const useApiFetch =  () => {
-    const dispatch = useDispatch();
-const dataFetch = useCallback(async (url) => {
-    const response = await fetch(url);
-    if (!response.ok){
-        throw new Error('something went wrong');
-    }
-    console.log('REQUESTED SEARCH')
-    const data = await response.json()
 
-    dispatch(apiActions.updateWeatherInfo({
-        city: data.name,
-        temperature: Math.round(data.main.temp),
-        weatherStatus: data.weather[0].description,
-        weatherMain: data.weather[0].main,
-        currentTime: data.dt,
-        sunsetTime: data.sys.sunset,
-        sunriseTime: data.sys.sunrise,
-        icon: data.weather[0].icon
-    }))
-},[dispatch])
-return {
-    requestData: dataFetch
-}
+    const dispatch = useDispatch();
+
+    const dataFetch = useCallback(async (url) => {
+        const response = await fetch(url);
+        if (!response.ok){
+            throw new Error('something went wrong');
+        }
+        console.log('REQUESTED SEARCH')
+        const data = await response.json()
+
+        dispatch(apiActions.updateWeatherInfo({
+            city: data.name,
+            temperature: Math.round(data.main.temp),
+            weatherStatus: data.weather[0].description,
+            weatherMain: data.weather[0].main,
+            currentTime: data.dt,
+            sunsetTime: data.sys.sunset,
+            sunriseTime: data.sys.sunrise,
+            icon: data.weather[0].icon
+        }))
+    },[dispatch])
+    return {
+        requestData: dataFetch
+    }
 }
 export default useApiFetch;
