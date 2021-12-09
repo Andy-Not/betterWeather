5d9cc02fd69eabd570afb784c8af17b7dd487623
joand <joandyalejogarcia@outlook.com>
2021-12-08 21:51:14 -0500

    error message

--- src/hooks/useApiFetch.js
+++ src/hooks/useApiFetch.js
@@ -9,11 +9,12 @@ const useApiFetch =  () => {
     const dataFetch = useCallback(async (url) => {
         const response = await fetch(url);
         if (!response.ok){
-            throw new Error('something went wrong');
+            dispatch(apiActions.updateWeatherInfo({
+                hasError: true
+            }))
+           return
         }
-        console.log('REQUESTED SEARCH')
         const data = await response.json()
-
         dispatch(apiActions.updateWeatherInfo({
             city: data.name,
             temperature: Math.round(data.main.temp),
@@ -22,7 +23,8 @@ const useApiFetch =  () => {
             currentTime: data.dt,
             sunsetTime: data.sys.sunset,
             sunriseTime: data.sys.sunrise,
-            icon: data.weather[0].icon
+            icon: data.weather[0].icon,
+            hasError: false
         }))
     },[dispatch])
     return {
